39ca326c941b950fdf5c12c789b5dd416f8fb5f6
Journey McDowell <jomcdowe@microsoft.com>
2020-10-15 21:39:54 -0700

    Initial commit with config input instead changing files

--- datamodeler.py
+++ datamodeler.py
@@ -26,20 +26,10 @@ import argparse
 import pickle
 from sklearn import preprocessing
 from sklearn.ensemble import GradientBoostingRegressor
-from conf_params_var import STATE_SPACE_DIM, ACTION_SPACE_DIM
-
-
+import yaml
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--use-gb", type=bool, default=False,help="choose gradient boosting as a model")
-parser.add_argument("--grid-search", type=bool, default=False, help="use grid search to tune hyperparameter tuning, if applicable")
-parser.add_argument("--genetic-search", type=bool, default=False, help="use genetic algorithn to tune hyperparameter if applicable")
-parser.add_argument("--use-lstm", type=bool, default=False, help="using lstm model after hyperparameter tuning")
-parser.add_argument("--use-nn", type=bool, default=False, help="choose multilayer perceptron as a model")
-parser.add_argument("--use-poly", type=bool, default=False, help="choose polynominal fitting as a model")
-#parser.add_argument("tune-ga", type=bool, default=False, help="uses genetic algorithm and TPOT library for hyperparameter tuning")
 parser.add_argument("--tune-rs", type=bool, default=False, help="uses random search from scikitlearn for hyperparameter tuning")
-
 
 
 def read_env_data():
@@ -57,13 +47,22 @@ if __name__=="__main__":
 
     args=parser.parse_args()
 
-    markovian_order=int(2)
+    with open('config/config_model.yml') as cmfile:
+        config = yaml.full_load(cmfile)
 
-    state_space_dim=int(STATE_SPACE_DIM)
-    action_space_dim=int(ACTION_SPACE_DIM)
+    state_space_dim = 0
+    action_space_dim = 0
+    for key, value in config['IO']['feature_name'].items():
+        if value == 'state':
+            state_space_dim += 1
+        elif value == 'action':
+            action_space_dim += 1
+        else:
+            print('Please fix config_model.yml to specify either state or action')
+            exit()
 
-    polydegree=int(1)
-
+    polydegree=int(config['POLY']['degree'])
+    markovian_order=int(config['LSTM']['markovian_order'])
 
     randomsearch_dist_lstm = {
         "activation": ['softmax', 'softplus', 'softsign', 'relu', 'tanh', 'sigmoid', 'hard_sigmoid', 'linear'],
@@ -101,7 +100,7 @@ if __name__=="__main__":
 
     x_set, y_set=read_env_data()
 
-    if args.use_nn==True:
+    if config['MODEL']['type'] == 'nn':
         scaler_x_set=preprocessing.MinMaxScaler(feature_range=(-1,1)).fit(x_set)
         scaler_y_set=preprocessing.MinMaxScaler(feature_range=(-1,1)).fit(y_set)
         joblib.dump(scaler_x_set, './models/scaler_x_set.pkl')
@@ -109,7 +108,7 @@ if __name__=="__main__":
         x_set=scaler_x_set.transform(x_set)
         y_set=scaler_y_set.transform(y_set)
 
-    if args.use_lstm==True:
+    if config['MODEL']['type'] == 'lstm':
         l=x_set.shape[0]
         m=x_set.shape[1]
         n=x_set.shape[2]
@@ -132,7 +131,7 @@ if __name__=="__main__":
 
 
     if args.tune_rs==True:
-        if args.use_lstm==True:
+        if config['MODEL']['type'] == 'lstm':
             model=KerasRegressor(build_fn=create_lstm_model_wrapper,epochs=10, batch_size=1024, verbose=1)
             random_search = RandomizedSearchCV(estimator=model, param_distributions=randomsearch_dist_lstm, n_iter=50, n_jobs=-1, cv=5)
             result = random_search.fit(x_train, y_train)
@@ -140,7 +139,7 @@ if __name__=="__main__":
             filename='./models/lstm_random_search_results_'+str(100*result.best_score_)+'.pkl'
             joblib.dump(result.best_params_, filename)
 
-        elif args.use_nn==True:
+        elif config['MODEL']['type'] == 'nn':
             model=KerasRegressor(build_fn=create_nn_model_wrapper,epochs=100, batch_size=1024, verbose=1)
             random_search = RandomizedSearchCV(estimator=model, param_distributions=random_search_nn, n_iter=50, n_jobs=-1, cv=5)
             result = random_search.fit(x_train, y_train)
@@ -176,7 +175,7 @@ if __name__=="__main__":
 
 
 
-    if args.use_gb==True and args.tune_rs==True:
+    if config['MODEL']['type'] == 'gb' and args.tune_rs==True:
         for i in range (0, y_set.shape[1]):
             gb_estimator=env_gb_modeler(state_space_dim=state_space_dim,action_space_dim=action_space_dim)
             gb_estimator.create_gb_model()
@@ -193,22 +192,32 @@ if __name__=="__main__":
             modelname='./models/gbmodel'+str(int(i))+'.sav'
             joblib.dump(model_opt, modelname)
 
-    elif args.use_gb==True and args.tune_rs==False:
+    elif config['MODEL']['type'] == 'gb' and args.tune_rs==False:
         print('using gradient boost regressor ....')
         for i in range (0, y_set.shape[1]):
-            gb_estimator=env_gb_modeler()
-            gb_estimator.create_gb_model()
+            gb_estimator=env_gb_modeler(
+                state_space_dim=state_space_dim,
+                action_space_dim=action_space_dim
+            )
+            gb_estimator.create_gb_model(
+                n_estimators=config['GB']['n_estimators'],
+                learning_rate=config['GB']['lr'],
+                max_depth=config['GB']['max_depth']
+            )
             gb_estimator.train_gb_model(x_train,y_train[:,i])
             score=gb_estimator.evaluate_gb_model(x_test, y_test[:,i])
             print('evaluation score is:', score)
             modelname='./models/gbmodel'+str(int(i))+'.sav'
             joblib.dump(gb_estimator.model, modelname)
 
-    if args.use_poly==True:
+    if config['MODEL']['type'] == 'poly':
         print('using polynomial fitting ....')
         for i in range (0, y_set.shape[1]):
-            poly_estimator=env_poly_modeler()
-            poly_estimator.create_poly_model(degree=polydegree)
+            poly_estimator=env_poly_modeler(
+                state_space_dim=state_space_dim,
+                action_space_dim=action_space_dim
+            )
+            poly_estimator.create_poly_model(degree=config['POLY']['degree'])
             poly_estimator.train_poly_model(x_train,y_train[:,i])
             score=poly_estimator.evaluate_poly_model(x_test, y_test[:,i])
             print('evaluation score is:', score)
@@ -225,22 +234,24 @@ if __name__=="__main__":
 # use the hyperparamter tuned network
 
 ##default neural network without hyperparamter tuning
-    if  args.tune_rs==False and args.use_lstm==True:
-        the_lstm_estimator=env_lstm_modeler()
-        config={"epochs": 100,
-                "batch_size": 512,
-                "activation":'linear',
-                "n_hidden_layer": 1,
-                "n_neuron": 12,
-                "lr": 10**-1,
-                "decay": 10**-3,
-                "dropout": 0.5,
-                "markovian_order":markovian_order, # should be >1 , zero order==>1
-                "num_lstm_units": 10}
-        the_lstm_estimator.create_model(config)
-        the_lstm_estimator.train_nn_model(x_train,y_train,config["epochs"],config["batch_size"])
+    if  args.tune_rs==False and config['MODEL']['type'] == 'lstm':
+        the_lstm_estimator=env_lstm_modeler(
+            state_space_dim=state_space_dim,
+            action_space_dim=action_space_dim
+        )
+        the_lstm_estimator.create_model(config['LSTM'])
+        the_lstm_estimator.train_nn_model(
+            x_train,
+            y_train,
+            config['LSTM']["epochs"],
+            config['LSTM']["batch_size"]
+        )
         lstmmodel=the_lstm_estimator.model
-        the_lstm_estimator.evaluate_nn_model(x_test, y_test,config["batch_size"])
+        the_lstm_estimator.evaluate_nn_model(
+            x_test,
+            y_test,
+            config['LSTM']["batch_size"]
+        )
         test_score=the_lstm_estimator.score[1]*100
         randomsample=np.random.random_integers(0,10,1)
         x_sample=x_set[randomsample]
@@ -254,20 +265,24 @@ if __name__=="__main__":
         modelname2='./models/lstmmodel.h5'
         lstmmodel.save(modelname2)
 
-    if args.tune_rs==False and args.use_nn==True:
-        nn_estimator=env_nn_modeler(state_space_dim=state_space_dim,action_space_dim=action_space_dim)
-        config={"epochs": 100,
-                "batch_size": 512,
-                "activation": 'linear',
-                "n_layer": 5,
-                "n_neuron": 12,
-                "lr": 10**-5,
-                "decay": 10**-7,
-                "dropout": 0.5}
-        nn_estimator.create_model(config)
-        nn_estimator.train_nn_model(x_train,y_train,config["epochs"],config["batch_size"])
+    if args.tune_rs==False and config['MODEL']['type'] == 'nn':
+        nn_estimator=env_nn_modeler(
+            state_space_dim=state_space_dim,
+            action_space_dim=action_space_dim
+        )
+        nn_estimator.create_model(config['NN'])
+        nn_estimator.train_nn_model(
+            x_train,
+            y_train,
+            config['NN']["epochs"],
+            config['NN']["batch_size"]
+        )
         nnmodel=nn_estimator.model
-        nn_estimator.evaluate_nn_model(x_test, y_test,config["batch_size"])
+        nn_estimator.evaluate_nn_model(
+            x_test,
+            y_test,
+            config['NN']["batch_size"]
+        )
         test_score=nn_estimator.score[1]*100
         randomsample=np.random.random_integers(0,10,1)
         x_sample=x_set[randomsample]
