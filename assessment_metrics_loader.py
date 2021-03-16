6ff69f1783f04ad671a953c60f81775e3b2ccaf5
Juan Vergara <juanvergara@microsoft.com>
2021-03-16 00:24:54 -0700

    Add validation routines
    Added sequential prediction from beginning to end;
    Added sequential prediction that recovers original X value after certain iterations/steps;
    Added evaluation functions for sequential analysis;
    Consolidated evaluation functions on new python file

---
+++ assessment_metrics_loader.py
@@ -0,0 +1,18 @@
+"""
+Any desired assessment metric should be added here
+
+"""
+# DEVELOPER TODO: Add your favorite assessing metrics
+from sklearn.metrics import mean_squared_error
+
+
+def root_mean_squared_error(y_true, y_pred, sample_weight = None, multioutput = 'uniform_average', squared = True):
+    squared = False
+    return mean_squared_error(y_true, y_pred, sample_weight, multioutput, squared)
+
+
+available_metrics = {
+    "mean_squared_error": mean_squared_error,
+    "root_mean_squared_error": root_mean_squared_error,
+}
+
