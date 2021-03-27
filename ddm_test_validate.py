f914cf64bfb4410aaf26bf5833a5353d834f1764
sandip.kulkarni <sandip.kulkarni@bons.ai>
2021-03-26 23:29:33 -0700

    deleted sim and modified based on comments

--- ddm_test_validate.py
+++ ddm_test_validate.py
@@ -2,20 +2,10 @@ import logging
 import datetime
 import pathlib
 import os
-import time
-from distutils.util import strtobool
-from typing import Any, Dict, List
-
 import numpy as np
 import pandas as pd
-from azure.core.exceptions import HttpResponseError
-from dotenv import load_dotenv, set_key
-from microsoft_bonsai_api.simulator.client import BonsaiClient, BonsaiClientConfig
-from microsoft_bonsai_api.simulator.generated.models import (
-    SimulatorInterface,
-    SimulatorSessionResponse,
-    SimulatorState,
-)
+from typing import Any, Dict, List
+import time
 
 from base import BaseModel
 
@@ -27,9 +17,9 @@ import hydra
 from omegaconf import DictConfig
 
 from model_loader import available_models
-
-from sim.moab.moab_main import SimulatorSession,  env_setup
-from sim.moab.policies import coast, random_policy
+## Add a local simulator in a `sim` folder to validate data-driven model
+## Example: Moab from a Microsoft Bonsai
+## from sim.moab.moab_main import SimulatorSession,  env_setup
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 env_name = "DDM"
@@ -38,18 +28,19 @@ log_path = "logs"
 
 class Simulator(BaseModel):
     def __init__(self, model, states=List[str], actions=List[str], configs=List[str], 
-        log_file: str = None,):
+        log_file: str = None,sim_orig=None):
         self.dd_model = model
         self.features = states + actions + configs
         self.labels = states
         self.config_keys = configs
         self.state_keys = states
         self.action_keys = actions
-        self.sim_physics = SimulatorSession()
+        self.sim_orig = sim_orig # include simulator function if comparing to simulator
+        
         if log_file=="enable":
             current_time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-            log_file = current_time + "_" + env_name + "_log.csv"
-            log_file2 = current_time + "_" + "SIM" + "_log.csv"
+            log_file = os.path.join(log_path, current_time + "_" + env_name + "_log.csv")
+            log_file2 = os.path.join(log_path, current_time + "_" + "SIMDATA" + "_log.csv")
             logs_directory = pathlib.Path(log_file).parent.absolute()
             if not pathlib.Path(logs_directory).exists():
                 print(
@@ -58,8 +49,8 @@ class Simulator(BaseModel):
                     )
                 )
                 logs_directory.mkdir(parents=True, exist_ok=True)
-        self.log_file = os.path.join(log_path, log_file)
-        self.log_file2 = os.path.join(log_path, log_file2)
+        self.log_file = log_file
+        self.log_file2 = log_file2 #os.path.join(log_path, log_file2)
 
     def episode_start(self, config: Dict[str, Any] = None):
         if config:
@@ -68,17 +59,22 @@ class Simulator(BaseModel):
             # configs randomized here. Need to decide a single place for configs
             # range either in main.py or in simulator configs
             self.config = {j:np.random.uniform(-1,1) for j in self.config_keys}
-        # Assign same state as would be used by simulator
-        self.sim_physics.episode_start(config)
-        _fullstate = self.sim_physics.get_state()
-        # idempotent dict comprehension for sims with prefix in configurations
-        self.state = {j:_fullstate[l] for j in self.state_keys for l in _fullstate.keys() if l in j}
+        if self.sim_orig:
+            # Assign same state as would be used by simulator
+            self.sim_orig.episode_start(config)
+            _fullstate = self.sim_orig.get_state()
+            # idempotent dict comprehension for sims with prefix in configurations
+            self.state = {j:_fullstate[l] for j in self.state_keys for l in _fullstate.keys() if l in j}
+        else:
+            # randomized state here need to be changed to appropriate ranges of each state
+            # see Table of All Data (TOAD) from Discovery Session
+            self.state = {j:np.random.uniform(-0.1,0.1) for j in self.state_keys}
         
 
     def episode_step(self, action: Dict[str, int]):
 
         input_list = [
-            list(self.state.values()),
+            list(self.state.values()), # 
             list(self.config.values()),
             list(action.values()),
         ]
@@ -93,16 +89,32 @@ class Simulator(BaseModel):
         return self.state
 
     def get_sim_state(self):
-        _fullstate = self.sim_physics.get_state()
-        # idempotent dict comprehension for sims with prefix in configurations
-        return {j:_fullstate[l] for j in self.state_keys for l in _fullstate.keys() if l in j}
-    
-    def get_init_sim_actions(self):
-        _fullstate = self.sim_physics.get_state()
-        _init_actions = {j:_fullstate[l] for j in self.action_keys for l in _fullstate.keys() if l in j}
+        if self.sim_orig:
+            _fullstate = self.sim_orig.get_state()
+            # idempotent dict comprehension for sims with prefix in configurations
+            return {j:_fullstate[l] for j in self.state_keys for l in _fullstate.keys() if l in j}
+        else:
+            return self.state
+
+    def get_init_sim_actions(self,_init_actions=None):
+        # If simulator exists, get initial actions from the sim
+        if self.sim_orig:
+            _fullstate = self.sim_orig.get_state()
+            _init_actions = {j:_fullstate[l] for j in self.action_keys for l in _fullstate.keys() if l in j}
         if not _init_actions:
+            # If simulator is unavailable or does not have initial actions, assign
+            # random actions, with range same as actuator limits. See TOAD from discovery
             _init_actions = {j:np.random.uniform(-1,1) for j in self.action_keys} 
         return _init_actions
+    
+    def test_policies(self,policy, action):
+        if policy == 'random':
+            return {j:np.random.uniform(-1,1) for j in self.action_keys}
+        elif policy == 'zeros':
+            return {j:0 for j in self.action_keys}
+        else: # coasting is default policy - no change in actions
+            return action
+        # TO DO: Add benchmark policy or other case specific scenarios 
 
     def halted(self):
         pass
@@ -122,7 +134,6 @@ class Simulator(BaseModel):
         data["episode"] = episode
         data["iteration"] = iteration
         log_df = pd.DataFrame(data, index=[0])
-        print(fname)
 
 
         if os.path.exists(fname):
@@ -132,7 +143,9 @@ class Simulator(BaseModel):
         else:
             log_df.to_csv(path_or_buf=fname, mode="w", header=True, index=False)
 
-def test_random_policy(
+    
+
+def test_sim_model(
     num_episodes: int = 100,
     num_iterations: int = 250,
     log_iterations: bool= True,
@@ -157,21 +170,25 @@ def test_random_policy(
         action = sim.get_init_sim_actions()
         if log_iterations:
             sim.log_iterations(ddm_state, action, sim.log_file, episode, iteration)
-            sim.log_iterations(sim_state, action, sim.log_file2, episode, iteration)
+            if sim.sim_orig:
+                sim.log_iterations(sim_state, action, sim.log_file2, episode, iteration)
         while not terminal:
-            action = random_policy(ddm_state)
+            action = sim.test_policies("random",action)
             # sim iteration
             sim.episode_step(action)
-            sim.sim_physics.episode_step(action)
             ddm_state = sim.get_state()
-            sim_state = sim.get_sim_state()
+            if sim.sim_orig:
+                sim.sim_orig.episode_step(action)
+                sim_state = sim.get_sim_state()
             iteration += 1
             if log_iterations:
                 sim.log_iterations(ddm_state, action, sim.log_file, episode, iteration)
-                sim.log_iterations(sim_state, action, sim.log_file2, episode, iteration)
+                if sim.sim_orig:
+                    sim.log_iterations(sim_state, action, sim.log_file2, episode, iteration)
             print(f"Running iteration #{iteration} for episode #{episode}")
             print(f"Observations for Sim: {sim_state}")
             print(f"Observations for Data: {ddm_state}")
+            # Add additional terminal conditions if required. Here only time-out is used.
             terminal = iteration >= num_iterations
 
     return sim
@@ -203,7 +220,7 @@ def main(cfg: DictConfig):
     # Grab standardized way to interact with sim API
     sim = Simulator(model, states, actions, configs, logflag)
     
-    test_random_policy(100, 250,logflag=="enable", sim)
+    test_sim_model(2, 250,logflag, sim)
     
     return sim
 
